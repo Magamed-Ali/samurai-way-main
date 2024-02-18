bec903a6ce62b9fefba56dc97c963bb9fe49033b
MuhammadAli <magamedali93@mail.ru>
2024-02-18 18:18:02 +0300

    lesson - 86, 87, 88 - theory: Virtual DOM, sholdComponent, PureComponent, React.memo, pure function

--- src/App.tsx
+++ src/App.tsx
@@ -16,29 +16,23 @@ import { compose } from 'redux';
 import {initializeApi} from "./redux/app-reducer";
 import {AppStateType} from "./redux/redux-store";
 import Loader from "./components/Loader/Loader";
+import svgImage from "./creativity_innovation_brainstorming_inspiration_vision_icon_260962.svg"
 
 
 class App extends  React.Component< {initialized: boolean, initializeApi: () => void }, any>{
 
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isActive: false // или true
+        };
+    }
     componentDidMount() {
 
         /** axios api*/
         this.props.initializeApi()
     }
     render() {
-
-        const arrCheck = (value: Array<any>) =>{
-
-            for(let i=0; i < value.length; i++){
-                if(Array.isArray(value[i])){
-                    return true
-                }
-            }
-            return false
-        }
-
-        console.log(arrCheck(['df','df']), "массив")
-
 
         if (!this.props.initialized) {
 
@@ -64,6 +58,13 @@ class App extends  React.Component< {initialized: boolean, initializeApi: () => 
                         <Route path="/settings" render={() => <Settings/>}/>
                         <Route path="/friends" render={() => <Friends/>}/>
                         <Route path="/login" render={() => <Login/>}/>
+                        {
+                            this.state.isActive ? <img onClick={() => this.setState({isActive: !this.state.isActive})}  src={svgImage} alt="SVG Image"
+                                            style={{filter: 'drop-shadow(2px 2px 2px yellow', width: "80px"}}/> :
+                                <img onClick={() => this.setState({isActive: !this.state.isActive})}  src={svgImage} alt="SVG Image"
+                                     style={{ width: "80px"}}/>
+                        }
+
                     </div>
                 </div>
             </>
@@ -72,6 +73,7 @@ class App extends  React.Component< {initialized: boolean, initializeApi: () => 
 
 
 }
+
 const mapStateToProps = (state: AppStateType) => ({
     initialized: state.appReducer.initialized
 })
